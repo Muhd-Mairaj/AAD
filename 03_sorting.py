cdf7a7f13cda3a5014957b7d79c957d70fbf16d1
Mairaj <rayyanmairaj2@gmail.com>
2025-07-20 00:09:34 +0800

    Add radix sort implementation with a modified counting sort

--- 03_sorting.py
+++ 03_sorting.py
@@ -21,9 +21,65 @@ def bubble_sort_adaptive(arr):
             break  # No swaps means the array is sorted already
 
 
+def counting_sort(arr):
+    k = max(arr)
+    count = [0] * (k+1)
+
+    # count the frequency of each element
+    for num in arr:
+        count[num] += 1
+
+    index = 0
+    for i in range(1, k+1):
+        # place the element based on its frequency
+        # e.g. if count[3] = 2, then 3 will be placed twice in the sorted array
+        while count[i] > 0:
+            arr[index] = i
+            index += 1
+            count[i] -= 1
+
+
+def counting_sort_by_digit(arr, exp):
+    count = [0] * 10  # For digits 0-9
+    output = [0] * len(arr)
+
+    # count occurrences of each digit at `exp` position
+    # e.g. if exp = 100, we count the tens place
+    # (2157 // 100) = 21, (21 % 10) = 1
+    for num in arr:
+        index = (num // exp) % 10
+        count[index] += 1
+
+    # compute the running sum
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    # build the output array (in reverse)
+    for i in reversed(range(len(arr))):
+        index = (arr[i] // exp) % 10
+        output[count[index] - 1] = arr[i]
+        count[index] -= 1
+
+    # copy the output array back to the original array
+    for i in range(len(arr)):
+        arr[i] = output[i]
+
+    
+def radix_sort(arr):
+    # find number of digits in the maximum number
+    max_num = max(arr)
+    digits = len(str(max_num))
+
+    for i in range(0, digits):
+        exp = 10 ** i  # Exponent for the current digit
+        counting_sort_by_digit(arr, exp)
+        
+
 if __name__ == "__main__":
     random.seed(42)  # For reproducibility
 
     sample_list = [random.randint(1, 100) for _ in range(random.randint(10, 20))]
-    bubble_sort(sample_list)
-    print("Bubble Sorted array:", sample_list)
+    print("Original array:", sample_list)
+
+    radix_sort(sample_list)
+    print("Sorted array:", sample_list)
